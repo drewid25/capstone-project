5b9232d151242687f86d15151e635eebaad795ad
Matthew Ervin Silvestre <matt09silvestre@gmail.com>
2022-09-07 23:46:39 +0800

    edit sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,20 +2,29 @@
 
    
 
-<div class="h-full border bg-accent-blue border-border-gray py-10 px-10  border-r-none">
+<div class="h-full border bg-accent-blue border-border-gray py-5 px-5  border-r-none">
         <div class = "flex flex-col">
+        
+        <div class="hover:bg-button-blue hover:text-white rounded-r-full text-left hover:indent-4 pt-3">
         <a href="/addproperty" class="text-letter font-bold">
-
-        <i class="fa-solid fa-sliders text-black mr-2 mb-5"></i> Add Property</a>
+        <i class="fa-solid fa-window-maximize text-grey mr-2 mb-5"></i> Add Property</a>
+        </div>
+        <div class="hover:bg-button-blue hover:text-white rounded-r-full text-left hover:indent-4 pt-3">
         <a href="/addemployee" class="text-letter font-bold">
-            <i class="fa-solid fa-user mr-2 mb-5"></i> Add Employee</a>
+            <i class="fa-sharp fa-solid fa-user-plus text-grey mr-2 mb-5"></i> Add Employee</a>
+        </div>
+        <div class="hover:bg-button-blue hover:text-white rounded-r-full text-left hover:indent-4 pt-3">
         <a href="/inventory" class="text-letter font-bold">
-            <i class="fa-solid fa-cart-flatbed mr-2 mb-5"></i></i> Inventory List</a>
+        <i class="fa-solid fa-warehouse text-grey mr-2 mb-5"></i></i> Inventory List</a>
+        </div>
+        <div class="hover:bg-button-blue hover:text-white rounded-r-full text-left hover:indent-4 pt-3">
         <a href="/employeelist" class="text-letter font-bold">
-        <i class="fa-solid fa-sliders text-black mr-2"></i> Employee List</a>
+        <i class="fa-solid fa-address-book text-grey mr-2 mb-5"></i> Employee List</a>
+        </div>
+        <div class="hover:bg-button-blue hover:text-white rounded-r-full text-left hover:indent-4 pt-3">
         <a href="#" class="text-letter font-bold">
-        <i class="fa-solid fa-sliders text-black mr-2"></i> Add Property</a>
-    
+        <i class="fa-solid fa-sliders text-grey mr-2 mb-5"></i> Add Property</a>
+        </div>
     </div>
 </div>
 
