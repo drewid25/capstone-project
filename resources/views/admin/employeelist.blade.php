9b6e1847add1e286e33ff46c48c041b1f3388a80
andrew <andrewidanol@gmai.com>
2022-09-17 11:49:46 +0800

    edited h-screen

--- resources/views/admin/employeelist.blade.php
+++ resources/views/admin/employeelist.blade.php
@@ -1,6 +1,6 @@
 @include('partials.header')
      <x-adminnav/>
-     <div class="container py-6 px-6 bg-white h-full mx-auto  grid grid-cols-12 grid-flow-col ">
+     <div class="container py-6 px-6 bg-white h-screen mx-auto  grid grid-cols-12 grid-flow-col ">
      <x-adminsidebar/>
     
     <x-employeelist :employees=$employees />
